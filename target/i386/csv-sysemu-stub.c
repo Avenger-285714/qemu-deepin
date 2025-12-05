pvpk1994/qemu

target/i386: csv: Release CSV3 shared pages after unmapping DMA
The shared pages are created for Device DMA access, release them
once DMA mapping is removed.

Signed-off-by: yangwencheng <yangwencheng@hygon.cn>

target/i386/csv-sysemu-stub.c
@@ -39,3 +39,8 @@ void csv3_shared_region_dma_unmap(uint64_t start, uint64_t end)
 {
 
 }
+
+void csv3_shared_region_relese(uint64_t gpa, uint32_t num_pages)
+{
+
+}

Avenger-285714/qemu-deepin

Signed-off-by: hanliyang <hanliyang@hygon.cn>

target/i386/csv-sysemu-stub.c
@@ -40,6 +40,11 @@ void csv3_shared_region_dma_unmap(uint64_t start, uint64_t end)
 
 }
 
+void csv3_shared_region_release(uint64_t gpa, uint32_t num_pages)
+{
+
+}
+
 int csv3_set_guest_private_memory(Error **errp)
 {
     g_assert_not_reached();